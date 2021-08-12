6d2e5d72caaff0eeed7f2940d55ab8941e201c3b
Elroy Toscano <andeltos@gmail.com>
2021-08-12 14:13:55 +0530

    change

--- src/components/Content/ProductContent/ProductContainer.js
+++ src/components/Content/ProductContent/ProductContainer.js
@@ -75,7 +75,7 @@ const ProductContainer = (props) => {
                 <div className='button-container'>
                   <button
                     css={`
-                    cursor:poin
+                      cursor: pointer;
                       margin-right: 2rem;
                       display: inline-block;
                       text-align: center;
